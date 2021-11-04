90901cbd20a9f1ee90ad72a19a539dfacfbc806b
julieta96 <julietaextra14@gmail.com>
2021-11-04 17:23:50 -0300

    update centro medico model

--- model/CentroMedicoModel.php
+++ model/CentroMedicoModel.php
@@ -13,41 +13,4 @@ class CentroMedicoModel
        return $this->database->consulta("SELECT * FROM centro_medico");
     }
 
-
-    public function getTurnosCentroMedico($centroMedico){
-        return $this->database->consulta("SELECT * FROM turno t
-                                          INNER JOIN centro_medico cm
-                                          ON cm.id_centro_medico= t.id_centro_medico
-                                          WHERE nom_centro_medico='$centroMedico'");
-    }
-
-    public function getCentroMedicoPorTurno($idTurno){
-        return $this->database->consulta("SELECT * FROM centro_medico cm
-                                          INNER JOIN turno t
-                                          ON cm.id_centro_medico= t.id_centro_medico
-                                          WHERE t.id_turno='$idTurno'");
-    }
-
-
-    public function getTurno($idTurno){
-        return $this->database->consulta("SELECT * FROM turno t
-                                          WHERE t.id_turno='$idTurno'");
-    }
-
-    public function getUsuarioPorTurno($usuario){
-        return $this->database->consulta("SELECT * FROM usuario u 
-                                          INNER JOIN turno t
-                                          ON t.usuario= u.id_usuario
-                                          WHERE t.usuario='$usuario'");
-    }
-
-
-    public function updateTurno($idTurno, $idUsuario){
-       return $this->database->update("UPDATE turno 
-                                        SET usuario='$idUsuario',
-                                            disponible=false
-                                        WHERE id_turno='$idTurno'");
-    }
-
-   
 }
