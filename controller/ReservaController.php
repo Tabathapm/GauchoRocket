a2f62c490a042847ca6abf9d151c2f4fedb61391
julieta96 <julietaextra14@gmail.com>
2021-11-14 20:01:44 -0300

    se adelanto algo de reservas

--- controller/ReservaController.php
+++ controller/ReservaController.php
@@ -4,10 +4,12 @@ class ReservaController{
 
     private $render;
     private $reservaModel;
+    private $pdf;
 
-    public function __construct(\Render $render, \ReservaModel $reservaModel){
+    public function __construct(\Render $render, \ReservaModel $reservaModel, \PDF $pdf){
         $this->render = $render;
         $this->reservaModel = $reservaModel;
+        $this->pdf=$pdf;
     }
 
 //    public function reservas(){
@@ -57,12 +59,23 @@ class ReservaController{
 
         $data = array();
 
+        $data['servicios'] = $this->reservaModel->servicios();
+        $data['cabinas'] = $this->reservaModel->cabinas();
+
         if (isset($_SESSION["logueado"])) {
             $data["logueado"] = $_SESSION["logueado"];
         }
 
+        if (isset($_SESSION["id"])) {
+            $data["id"] = $_SESSION["id"];
+        }
+
         if (isset($_SESSION["nombre"])) {
             $data["nombre"] = $_SESSION["nombre"];
+        }
+
+        if (isset($_SESSION["apellido"])) {
+            $data["apellido"] = $_SESSION["apellido"];
         }
 
         if (isset($_SESSION["esAdmin"])) {
@@ -73,11 +86,63 @@ class ReservaController{
             $data["esClient"] = $_SESSION["esClient"];
         }
 
+        $data['tipoVuelo'] = $this->reservaModel->getResultadoChequeo($_SESSION["id"]);
+
         if (isset($data["logueado"])) {
             echo $this->render->renderizar("view/reservas.mustache", $data);
         }else{
             echo $this->render->renderizar("view/login.mustache");
         }
+    }
+
+
+    public function crearPDF($datosVuelo){
+
+        $nombre=$datosVuelo[0]['nombre'];
+        $apellido=$datosVuelo[0]['apellido'];
+        $horarioReserva=$datosVuelo[0]['horaReserva'];
+
+        $message ="
+         <div>
+            <h1>Gaucho Rocket</h1>
+            <div>
+                <p>
+                  Reserva realizada por: $nombre $apellido,
+
+                  Para el vuelo en el dia en el horario
+                  
+                </p>
+            </div>
+         </div>
+
+         ";
+
+         $data['pdf']=$this->pdf->createPDF($message,'reserva');
+
+         echo $this->render->renderizar("view/pdf.mustache", $data);
+     }
+
+
+    public function generarReserva(){
+
+        $comprobanteReserva= substr(md5(uniqid(rand(),true)),0,8);
+
+        $usuario = $_SESSION["id"];
+        $nombre = $_SESSION["nombre"];
+        $apellido = $_SESSION["apellido"];
+
+        $servicio = $_POST['servicio'];
+        $cabina = $_POST['cabina'];
+
+        $horaReserva = time();
+
+        $servicioEncontrado= $this->reservaModel->getServicio($servicio);
+        $cabinaEncontrada = $this->reservaModel->getCabina($cabina);
+
+        $guardarReserva= $this->reservaModel->registrarReserva($horaReserva,'123', $id_vuelo,$servicioEncontrado[0]['id_tipo_servicio'] ,$cabinaEncontrada[0]['id_cabina'], $usuario);
+
+
+        echo $this->render->renderizar("view/miReserva.mustache");
     }
 
 
