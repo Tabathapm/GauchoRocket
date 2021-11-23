4e6e0225d2cf9ae47004f6f3421e35caad153cfb
julieta96 <julietaextra14@gmail.com>
2021-11-22 21:08:52 -0300

    ya casi esta reserva

--- controller/ReservaController.php
+++ controller/ReservaController.php
@@ -46,6 +46,22 @@ class ReservaController{
              $data['cabinas'] = $this->reservaModel->cabinas();
              $data['tipoVuelo'] = $this->reservaModel->getResultadoChequeo($_SESSION["id"]);
 
+             if(isset($_POST['idViaje']) && isset($_POST['destino'])
+                && isset($_POST['horario']) && isset($_POST['fecha'])
+                && isset($_POST['precio']) && isset($_POST['foto'])
+                && isset($_POST['duracion']) && isset($_POST['vuelo']) ){
+
+                $viaje=$_POST['idViaje'];
+                $data['viaje'] = $this->reservaModel->getViaje($viaje);
+                $data['destino'] = $_POST['destino'];
+                $data['horario'] = $_POST['horario'];
+                $data['fecha'] = $_POST['fecha'];
+                $data['precio'] = $_POST['precio'];
+                $data['foto'] = $_POST['foto'];
+                $data['duracion'] = $_POST['duracion'];
+                $data['vuelo'] = $_POST['vuelo'];
+             }
+            
             echo $this->render->renderizar("view/reservas.mustache", $data);
 
         }else{
@@ -58,37 +74,37 @@ class ReservaController{
     }
 
 
-    public function crearPDF($datosVuelo){
+    public function crearPDF(){
 
-        $nombre=$datosVuelo[0]['nombre'];
-        $apellido=$datosVuelo[0]['apellido'];
-        $horarioReserva=$datosVuelo[0]['horaReserva'];
-
-
+        $nombre= $_SESSION["nombre"];
+        $apellido= $_SESSION["apellido"];
+        /*$horarioReserva= $_POST['hora'];
+        $cabina=$_POST['cabina'];
+        $servicio=$_POST['servicio'];
+        $vuelo=$_POST['vuelo'];*/
+       
+        $host = "http://".$_SERVER['HTTP_HOST'];
+      
         $message ="
          <div>
-            <h1>Gaucho Rocket</h1>
+            <img src='$host/GauchoRocket/public/images/marca-pdf.png' style='width:60rem;'/>
             <div>
                 <p>
-                  Reserva realizada por: $nombre $apellido,
-
-                  Para el vuelo en el dia en el horario
-                  
+                  Reserva realizada por:".$nombre." ".$apellido.",
                 </p>
             </div>
-         </div>
-
-         ";
+         </div>";
 
          $data['pdf']=$this->pdf->createPDF($message,'reserva');
 
         if(isset($_SESSION['logueado'])){
-            echo $this->render->renderizar("view/reservas.mustache");
+            echo $this->render->renderizar("view/pdf.mustache");
         }
         else{
             header("Location: /GauchoRocket/login");
+            exit();
         }
-
+        
          echo $this->render->renderizar("view/pdf.mustache", $data);
      }
 
@@ -106,11 +122,23 @@ class ReservaController{
 
         $horaReserva = time();
 
+        $id_vuelo = $_POST['vuelo'];
+
         $servicioEncontrado= $this->reservaModel->getServicio($servicio);
         $cabinaEncontrada = $this->reservaModel->getCabina($cabina);
 
-        $guardarReserva= $this->reservaModel->registrarReserva($horaReserva,'123', $id_vuelo,$servicioEncontrado[0]['id_tipo_servicio'] ,$cabinaEncontrada[0]['id_cabina'], $usuario);
+        if($this->reservaModel->registrarReserva($horaReserva,'123', $id_vuelo,$servicioEncontrado[0]['id_tipo_servicio'] ,$cabinaEncontrada[0]['id_cabina'], $usuario)){
 
+            $data['horaReserva'] = $horaReserva;
+            $data['servicio'] = $servicioEncontrado[0]['id_tipo_servicio'];
+            $data['cabina'] = $cabinaEncontrada[0]['id_cabina'];
+            $data['vuelo'] = $id_vuelo;
+            $data['reservaRegistrada']=true;
+
+        }else{
+            $data['reservaRegistrada']=false;
+            $data['mensaje']='Hubo un error, intente mas tarde!';
+        }
 
         echo $this->render->renderizar("view/miReserva.mustache");
 
@@ -120,6 +148,9 @@ class ReservaController{
 
 
 }
+
+
+
 
 
 
--- model/ReservaModel.php
+++ model/ReservaModel.php
@@ -11,9 +11,9 @@ class ReservaModel
 
 
     public function registrarReserva($hora_reserva, $id_tarjeta, $id_vuelo,$id_tipo_servicio, $id_cabina, $id_usuario){
-        return $this->database->ejecutar("INSERT INTO reserva(hora_reserva,id_tarjeta,id_vuelo,id_tipo_servicio,id_cabina)
+        return $this->database->update("INSERT INTO reserva(      hora_reserva,id_tarjeta,id_vuelo,id_tipo_servicio,id_cabina, id_usuario)
                                            VALUES
-                                          ('$id_reserva','$hora_reserva','$id_tarjeta','$id_vuelo','$id_tipo_servicio','$id_cabina','$id_usuario')");
+                                          ('$hora_reserva','$id_tarjeta','$id_vuelo','$id_tipo_servicio','$id_cabina','$id_usuario')");
     }
 
     public function servicios(){
@@ -26,20 +26,27 @@ class ReservaModel
 
     public function getServicio($id){
         return $this->database->consulta("SELECT * FROM tipo_servicio_a_bordo
-        								  WHERE id_tipo_servicio='$id'");
+                                          WHERE id_tipo_servicio='$id'");
     }
 
     public function getCabina($id){
         return $this->database->consulta("SELECT * FROM cabina
-        								  WHERE id_cabina ='$id'");
+                                          WHERE id_cabina ='$id'");
+    }
+
+    public function getViaje($id){
+        return $this->database->consulta("SELECT * FROM viaje vi
+                                          INNER JOIN vuelo vu
+                                          ON vi.id_viaje = vu.id_viaje
+                                          WHERE vi.id_viaje ='$id'");
     }
 
     public function getResultadoChequeo($id_usuario){
 
-    	return $this->database->consulta("SELECT resultado FROM chequeo_medico cm
-    									  INNER JOIN turno t
+        return $this->database->consulta("SELECT resultado FROM chequeo_medico cm
+                                          INNER JOIN turno t
                                           ON cm.turno= t.id_turno
-        								  WHERE t.usuario ='$id_usuario'");
+                                          WHERE t.usuario ='$id_usuario'");
 
     }
 
