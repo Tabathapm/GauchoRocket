4ebe1b01c3ccf53925a9dd89d30f7d5d3b5f35c0
Tabathapm <tabathapm@gmail.com>
2021-11-28 14:12:40 -0300

    consultas para la busqueda de alojamientos

--- model/AlojamientoModel.php
+++ model/AlojamientoModel.php
@@ -13,30 +13,39 @@ class AlojamientoModel{
                                                ");
     }
 
+    public function getAlojamiento($destino, $cantHabitacion){
+        return $this->database->consulta("SELECT *
+                                              FROM alojamiento
+                                              INNER JOIN destino on alojamiento.id_destino = destino.id_destino
+                                              WHERE destino.id_destino = '$destino' AND cant_habitaciones = '$cantHabitacion';");
+    }
+
+    public function getAlojamientoPorHabitacion($cantHabitacion){
+        return $this->database->consulta("SELECT *
+                                              FROM alojamiento
+                                              INNER JOIN destino on alojamiento.id_destino = destino.id_destino
+                                              WHERE cant_habitaciones = '$cantHabitacion'");
+    }
+
+    public function getAlojamientoPorDestino($destino){
+        return $this->database->consulta("SELECT *
+                                              FROM alojamiento
+                                              INNER JOIN destino on alojamiento.id_destino = destino.id_destino
+                                              WHERE destino.id_destino = '$destino'");
+    }
+
+    public function getTodosLosAlojamientos(){
+        return $this->database->consulta("SELECT *
+                                              FROM alojamiento
+                                              INNER JOIN destino 
+                                              ON alojamiento.id_destino = destino.id_destino");
+    }
+
     public function getAlojamientoConHabitacion($cant_habitaciones, $destino){
         return $this->database->consulta("select * from alojamiento  a   
                                                         inner join destino d on a.id_destino = d.id_destino
                                                         WHERE a.CANT_HABITACIONES = '$cant_habitaciones' AND d.descripcion = '$destino'");
-}
-
-
-//    public function getAlojamientoCon2Habitaciones($cant_habitaciones){
-//        return $this->database->consulta("SELECT * FROM ALOJAMIENTO WHERE CANT_HABITACIONES = '$cant_habitaciones'");
-//    }
-//
-//    public function getAlojamientoCon3Habitaciones($cant_habitaciones){
-//        return $this->database->consulta("SELECT * FROM ALOJAMIENTO WHERE CANT_HABITACIONES = '$cant_habitaciones'");
-//    }
-//
-//    public function getAlojamientoCon4Habitaciones($cant_habitaciones){
-//        return $this->database->consulta("SELECT * FROM ALOJAMIENTO WHERE CANT_HABITACIONES = '$cant_habitaciones'");
-//    }
-
-
-
-
-
-
+    }
 
 
 
