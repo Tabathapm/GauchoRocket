f9776b0f9b2b5d6729abcee91f4745f554820025
julieta96 <julietaextra14@gmail.com>
2021-11-29 19:40:36 -0300

    update pdf

--- helpers/PDF.php
+++ helpers/PDF.php
@@ -22,18 +22,17 @@ class PDF{
 		$this->pdf->loadHtml($data);
 		$this->pdf->setPaper('A4', 'landscape'); //size: A4 , orientation: landscape or portrait
 		$this->pdf->render();
-
-		$this->downloadPDF($this->pdf, $namePdf);
+	    $this->downloadPDF($this->pdf, $namePdf);
 
 	}
 
 
 	public function downloadPDF($pdf, $name){
 
-		$pdf->stream($name.'.pdf', ['Attachment'=>0]);
-
+		$pdf->stream($name.'.pdf');
 	}
+
+
+   
 	
 }
-
-
