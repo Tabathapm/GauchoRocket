86af276ad03e70c3050e25142a75245776acafb7
Tabathapm <tabathapm@gmail.com>
2021-12-06 18:08:27 -0300

    modificacion en los graficos del lado del admin

--- model/ReportesModel.php
+++ model/ReportesModel.php
@@ -9,20 +9,29 @@ class ReportesModel{
     }
 
     public function graficoViajes(){
-        return $this->database->consulta("SELECT *, DATE_FORMAT(f_partida, '%d/%m/%Y') AS 'fechaDeViaje', origen.descripcion AS 'Origen', destino.descripcion AS 'Destino', COUNT(viaje.disponible) AS 'cantVendido', cabina.tipo AS 'tipoDeCabina'
-                                              FROM origen
-                                              INNER JOIN vuelo
-                                              ON origen.id_origen = vuelo.vuelo_origen
-                                              INNER JOIN destino
-                                              ON destino.id_destino = vuelo.vuelo_destino
-                                              INNER  JOIN viaje
-                                              ON vuelo.id_viaje = viaje.id_viaje
-                                              INNER JOIN tipo_viaje
-                                              ON viaje.id_tipo_viaje = tipo_viaje.id_tipo_viaje
+        return $this->database->consulta("SELECT COUNT(reserva.id_cabina) AS 'cantVendido', cabina.tipo AS 'tipoDeCabina'
+                                              FROM reserva
                                               INNER JOIN cabina
-                                              ON vuelo.id_cabina = cabina.id_cabina
-                                              WHERE viaje.disponible = false 
-                                              GROUP BY cabina.id_cabina;");
+                                              ON reserva.id_cabina = cabina.id_cabina
+                                              WHERE pagado = FALSE 
+                                              GROUP BY cabina.tipo;");
     }
+
+//    public function graficoViajes(){
+//        return $this->database->consulta("SELECT *, DATE_FORMAT(f_partida, '%d/%m/%Y') AS 'fechaDeViaje', origen.descripcion AS 'Origen', destino.descripcion AS 'Destino', COUNT(viaje.disponible) AS 'cantVendido', cabina.tipo AS 'tipoDeCabina'
+//                                              FROM origen
+//                                              INNER JOIN vuelo
+//                                              ON origen.id_origen = vuelo.vuelo_origen
+//                                              INNER JOIN destino
+//                                              ON destino.id_destino = vuelo.vuelo_destino
+//                                              INNER  JOIN viaje
+//                                              ON vuelo.id_viaje = viaje.id_viaje
+//                                              INNER JOIN tipo_viaje
+//                                              ON viaje.id_tipo_viaje = tipo_viaje.id_tipo_viaje
+//                                              INNER JOIN cabina
+//                                              ON vuelo.id_cabina = cabina.id_cabina
+//                                              WHERE viaje.disponible = false
+//                                              GROUP BY cabina.id_cabina;");
+//    }
 
 }
